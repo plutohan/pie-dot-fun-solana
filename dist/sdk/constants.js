2617cbc06307c3620848dcac5d6f8cce11bd063f
pluto <pluto@ao.xyz>
2025-02-04 16:17:53 +0900

    build sdk

--- dist/sdk/constants.js
+++ dist/sdk/constants.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SYS_DECIMALS = exports.QUICKNODE_RPC_URL = void 0;
-exports.QUICKNODE_RPC_URL = "https://newest-solemn-feather.solana-mainnet.quiknode.pro/ed2fb25954d13154ec81f79728b1d6635f521568/";
+exports.SYS_DECIMALS = exports.JITO_RPC_URL = exports.QUICKNODE_RPC_URL = void 0;
+exports.QUICKNODE_RPC_URL = "https://newest-solemn-feather.solana-mainnet.quiknode.pro/ed2fb25954d13154ec81f79728b1d6635f521568";
+exports.JITO_RPC_URL = "https://mainnet.block-engine.jito.wtf/api/v1";
 exports.SYS_DECIMALS = 10 ** 6;
 //# sourceMappingURL=constants.js.map
