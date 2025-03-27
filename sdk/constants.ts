667772c1ee2d5ea98d7fd435ed200dc2ffc58fc4
pluto <pluto@ao.xyz>
2025-03-27 17:19:15 +0900

    fix: use default tip when jito tip API is down

--- sdk/constants.ts
+++ sdk/constants.ts
@@ -4,3 +4,5 @@ export const JITO_TIP_FLOOR_URL =
 export const JITO_RPC_URL = "https://mainnet.block-engine.jito.wtf/api/v1";
 
 export const SYS_DECIMALS = 10 ** 6;
+
+export const JITO_TIP_AMOUNT = 1000000; // 0.001 SOL
