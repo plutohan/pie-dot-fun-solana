17dad5e6b3d429acef9fb99919dd7b7aa6e62af8
pluto <pluto@ao.xyz>
2025-05-07 22:19:24 +0900

    refactor: move raydium functions to deprecated

--- programs/pie/src/instructions/deprecated/mod.rs
+++ programs/pie/src/instructions/deprecated/mod.rs
@@ -21,3 +21,12 @@ pub use sell_component_clmm::*;
 
 pub mod withdraw_component;
 pub use withdraw_component::*;
+
+pub mod execute_rebalancing;
+pub use execute_rebalancing::*;
+
+pub mod execute_rebalancing_cpmm;
+pub use execute_rebalancing_cpmm::*;
+
+pub mod execute_rebalancing_clmm;
+pub use execute_rebalancing_clmm::*;
